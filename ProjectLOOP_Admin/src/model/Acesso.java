1abb6f5acae4f1fe8530c83d8747872624f5c45e
Leonardo Bezerra <leonardobezerra39@gmail.com>
2017-09-30 13:34:10 -0300

    Add do project Admin

---
+++ ProjectLOOP_Admin/src/model/Acesso.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author leonardo
+ */
+public class Acesso {
+
+    private final String email;
+    private final String senha;
+    private final String tableAcesso = "Admin";
+    private String message;
+    
+    private boolean acesso;
+    
+    private int UserId;
+
+    public Acesso(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+        this.fazerLogin();
+    }
+    
+    private void fazerLogin() {
+        if (verificarEmail()) {       
+            
+            Connection con = ConnectionFactory.getConnection();
+            PreparedStatement stmt = null;
+
+            ResultSet rs = null;
+
+                try {
+                    
+                    String sql = "SELECT id FROM " + this.tableAcesso + " WHERE email = ? and senha = ?";
+                    
+                    stmt = con.prepareStatement(sql);
+                    stmt.setString(1, this.email);
+                    stmt.setString(2, this.senha);
+
+                    rs = stmt.executeQuery();
+
+                    if (rs.next()) { // Se selecionar tupla, usuario logado
+                        this.UserId = rs.getInt("id"); // Sessao 
+                        this.acesso = true;
+                    } else {
+                        this.message = "Senha incorreta";
+                        this.acesso = false;
+                    }
+
+                } catch (SQLException ex) {
+                    JOptionPane.showMessageDialog(null, "Erro ao Realizar Login" + ex);
+                    this.message = "Erro ao Realizar Login";
+                    this.acesso = false;
+                } finally {
+                    ConnectionFactory.closeConnection(con, stmt, rs);
+                }
+
+        } else {
+            this.message = "Email não está cadastrado";
+            this.acesso = false;
+        }
+    }
+
+    private boolean verificarEmail() {
+
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+
+        ResultSet rs = null;
+
+        boolean check = false;
+
+        try {
+
+            String sql = "SELECT id FROM " + this.tableAcesso + " WHERE email = ?";
+
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, this.email);
+
+            rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                check = true;
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao Realizar Login" + ex);
+            this.message = "Erro ao Realizar Login";
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt, rs);
+        }
+
+        return check;
+
+    }
+        
+    public int getUserId(){
+        return this.UserId;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+    
+    public boolean getAcesso(){
+        return this.acesso;
+    }
+
+}
---
+++ ProjectLOOP_Admin/src/projectloop/ProjectLOOP.java
@@ -0,0 +1,30 @@
+
+package projectloop;
+
+//import model.Acesso;
+
+import model.Acesso;
+import view.TelaLogin;
+//import model.Musica;
+
+
+
+/**
+ *
+ * @author leonardo
+ */
+public class ProjectLOOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        TelaLogin telaLogin = new TelaLogin();
+        
+        telaLogin.setVisible(true);
+        
+    }
+    
+}

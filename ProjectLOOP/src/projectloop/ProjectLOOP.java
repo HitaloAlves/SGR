d7e7c20fc2ef8dc769634bc2c42ad1613943bc3e
Leonardo Bezerra <leonardobezerra39@gmail.com>
2017-09-30 14:33:28 -0300

    Construção tela de login, e copia de views para ProjectLOOP

--- ProjectLOOP/src/projectloop/ProjectLOOP.java
+++ ProjectLOOP/src/projectloop/ProjectLOOP.java
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
+
 package projectloop;
 
-//import model.Acesso;
-
-import java.io.File;
-import model.Acesso;
-//import model.Musica;
+import view.TelaLogin;
 
 
 
@@ -21,84 +17,10 @@ public class ProjectLOOP {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        Acesso login = new Acesso("hitaloS@gmail.com", "abcd-2017", 0);
+        TelaLogin telaLogin = new TelaLogin();
         
-        System.out.println(login.getMessage());
-        
-//        Musica music = new Musica();
-//        
-//        music.playMusica("01");
-
-          
-          new File("musicas//Leonardo").mkdir(); // Criar Diretorio
+        telaLogin.setVisible(true);
         
     }
     
 }
-=======
-
-package projectloop;
-
-//import model.Acesso;
-
-import model.Musica;
-
-
-
-/**
- *
- * @author leonardo
- */
-public class ProjectLOOP {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        //Acesso login = new Acesso("leonardobezerra39@gmail.com", "a1l-swko", 0);
-        
-        //System.out.println(login.getMessage());
-        
-        Musica music = new Musica();
-        
-        music.playMusica("01");
-        
-    }
-    
-}
-=======
-
-package projectloop;
-
-//import model.Acesso;
-
-import model.Musica;
-
-
-
-/**
- *
- * @author leonardo
- */
-public class ProjectLOOP {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        //Acesso login = new Acesso("leonardobezerra39@gmail.com", "a1l-swko", 0);
-        
-        //System.out.println(login.getMessage());
-        
-        Musica music = new Musica();
-        
-        music.playMusica("01");
-        
-    }
-    
-}
->>>>>>> 125eae0366e311b6a06fe7e8466f82ad936982c3
